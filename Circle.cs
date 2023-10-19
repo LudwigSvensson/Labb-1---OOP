a64729d74019976000843dba8b33559d8e9f8e04
Ludwig Svensson <ludwig.gifrod@hotmail.com>
2023-10-19 13:17:51 +0200

    Add project files.

---
+++ Circle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb_1___OOP
+{
+    public class Circle
+    {
+        public float _pi = 3.141f;
+        public int _radius;
+        public Circle(int radius)
+        {
+            _radius = radius;            
+        }       
+        public void getArea()
+        {
+             
+            float area = _radius * _radius * _pi;
+            Console.WriteLine($"Om radien på cirkeln är {_radius} så blir arean: " + area);
+        }
+        public void getOmkrets ()
+        {
+            float O = (2 * _pi * _radius);                        
+            Console.WriteLine($"Om radien på cirkeln är {_radius}, så är omkretsen: {O}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Labb_1___OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {                        
+                Circle circle1 = new Circle(5);
+                circle1.getArea();
+                circle1.getOmkrets();
+            
+                Circle circle2 = new Circle(6);
+                circle2.getArea();
+                circle2.getOmkrets();            
+        }        
+    }    
+}
